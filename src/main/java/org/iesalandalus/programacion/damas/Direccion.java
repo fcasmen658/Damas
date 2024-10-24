b44b5942deefe0543b07c7afa975afca83be7627
Francisco M.C.M. <fcasmen658@gmail.com>
2024-10-24 23:53:29 +0200

    Creado enum 'Dirección'

---
+++ src/main/java/org/iesalandalus/programacion/damas/Direccion.java
@@ -0,0 +1,15 @@
+package org.iesalandalus.programacion.damas;
+
+public enum Direccion {
+    NORESTE("Noreste"), SURESTE("Sureste"), SUROESTE("Suroeste"), NOROESTE("Noroeste");
+
+    private String cadenaAMostrar;
+    private Direccion(String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return "Direccion{" + "cadenaAMostrar='" + cadenaAMostrar + '\'' + '}';
+    }
+}
